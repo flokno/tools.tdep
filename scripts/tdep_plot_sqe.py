0cd43356fe01605ca67b95fbd11d9bf6fe800b56
Florian Knoop <florian.knoop@liu.se>
2022-02-08 17:36:38 +0100

    package | add library/package, externalize functions for working with SQE

--- scripts/tdep_plot_sqe.py
+++ scripts/tdep_plot_sqe.py
@@ -1,15 +1,59 @@
 #! /usr/bin/env python
 from pathlib import Path
 
-import h5py as h5
 import matplotlib.pyplot as plt
 import numpy as np
+import rich
 import typer
 from matplotlib.colors import LogNorm, Normalize
 
+from tdeptools.sqe import find_ylim, get_arrays, get_arrays_dispersion
+
+echo = rich.print
+
 
 def get_canvas():
+    """set up canvas"""
     fig, ax = plt.subplots()
+    return fig, ax
+
+
+def plot(
+    x: np.ndarray,
+    y: np.ndarray,
+    gz: np.ndarray,
+    xt: np.ndarray,
+    xl: list,
+    yl: list,
+    ylim: float = None,
+    linear: bool = False,
+    ys_dispersion: np.ndarray = None,
+):
+    fig, ax = get_canvas()
+
+    if linear:
+        norm = Normalize(vmin=gz.min(), vmax=gz.max())
+    else:
+        norm = LogNorm(vmin=gz.min(), vmax=gz.max())
+    kw = {"cmap": "viridis", "shading": "auto", "norm": norm}
+
+    # for plotting, turn the axes into 2d arrays
+    gx, gy = np.meshgrid(x, y)
+
+    ax.pcolormesh(gx, gy, gz, **kw)
+    # set the limits of the plot to the limits of the data
+    ax.set_xlim([x.min(), x.max()])
+
+    ax.set_ylim([y.min(), ylim])
+    ax.set_xticks(xt)
+    ax.set_xticklabels(xl)
+    ax.set_ylabel(yl)
+
+    if ys_dispersion is not None:
+        kw = {"color": "white", "ls": "--", "lw": 0.5, "alpha": 0.5}
+        for y in ys_dispersion.T:
+            ax.plot(x, y, **kw)
+
     return fig, ax
 
 
@@ -24,78 +68,40 @@ def main(
     max_intensity: float = 1,
     min_intensity: float = 1e-4,
     linear: bool = False,
+    dispersion: Path = None,
 ):
-    # open the sqe file
-    typer.echo(f"Read spectral function from {file}")
-    f = h5.File(file, "r")
 
-    # typer.echo(".. keys in file:")
-    # typer.echo(f.keys())
-
-    # template output file name
-    outfile = file.stem
-
-    # get axes and intensity
-    x = np.array(f.get("q_values"))
-    y = np.array(f.get("energy_values"))
-    try:
-        gz = np.array(f["spectral_function"])
-    except KeyError:
-        gz = np.array(f["intensity"])  # compatibility with older sqe.hdf5 files
+    # read data
+    x, y, gz, xt, xl, yl = get_arrays(file)
+    if dispersion is not None:
+        _, ys_dispersion = get_arrays_dispersion(dispersion)
 
     # integrate intensity in energy
     n_bands = np.trapz(gz, x=y, axis=0).mean()
-    typer.echo(f".. no. of bands:      {n_bands}")
+    echo(f".. no. of bands:      {n_bands:.2f}")
 
-    if not ylim and max_frequency < 1:
-        # find ylim as fraction of full band occupation
-        for nn, yy in enumerate(y):
-            gz_int = np.trapz(gz[:nn], x=y[:nn], axis=0).mean()
-            if gz_int > max_frequency * n_bands:
-                ylim = yy
-                typer.echo(f".. {max_frequency*100}% intensity at {yy:.3f} THz")
-                break
+    if not ylim:
+        ylim = find_ylim(y, gz, max_frequency)
 
     # normalize intensity
     gz /= gz.max()
 
     # add a little bit so that the logscale does not go nuts
     gz = gz + min_intensity
-    # for plotting, turn the axes into 2d arrays
-    gx, gy = np.meshgrid(x, y)
-    # x-ticks
-    xt = np.array(f.get("q_ticks"))
-    # labels for the x-ticks
-    xl = f.attrs.get("q_tick_labels").decode().split()
-    # label for y-axis
-    yl = f"Energy ({f.attrs.get('energy_unit').decode():s})"
 
     # cap intensity
     if max_intensity < 1:
         gz[gz > max_intensity] = max_intensity
 
-    fig, ax = get_canvas()
-
-    if linear:
-        norm = Normalize(vmin=gz.min(), vmax=gz.max())
-    else:
-        norm = LogNorm(vmin=gz.min(), vmax=gz.max())
-    kw = {"cmap": "viridis", "shading": "auto", "norm": norm}
-    ax.pcolormesh(gx, gy, gz, **kw)
-    # set the limits of the plot to the limits of the data
-    ax.set_xlim([x.min(), x.max()])
-    if ylim is None:
-        ylim = y.max()
-
-    ax.set_ylim([y.min(), ylim])
-    ax.set_xticks(xt)
-    ax.set_xticklabels(xl)
-    ax.set_ylabel(yl)
+    fig, _ = plot(x, y, gz, xt, xl, yl, ylim, linear, ys_dispersion)
 
     # talk
-    typer.echo(f".. ylim is            {ylim:.4f} THz")
-    typer.echo(f".. max intensity:     {max_intensity}")
-    typer.echo(f".. use linear scale:  {linear}")
+    echo(f".. ylim is            {ylim:.4f} THz")
+    echo(f".. max intensity:     {max_intensity}")
+    echo(f".. use linear scale:  {linear}")
+
+    # template output file name
+    outfile = file.stem
 
     if max_intensity < 1:
         outfile += "_intensity"
@@ -105,7 +111,7 @@ def main(
         outfile += f"_max_{max_frequency:4.2f}"
 
     outfile += ".png"
-    typer.echo(f".. save to {outfile}")
+    echo(f".. save to {outfile}")
     fig.savefig(outfile, dpi=300)
 
 
---
+++ tdeptools/sqe.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+import imp
+from collections import namedtuple
+from pathlib import Path
+
+import h5py as h5
+import numpy as np
+import rich
+from rich import traceback
+
+traceback.install(show_locals=True)
+
+_arrays = ["q", "energy", "intensity", "ticks", "ticklabels", "unit"]
+data_spectral_cls = namedtuple("spectral_data", _arrays)
+data_dispersion_cls = namedtuple("dispersion_data", ["q", "energy"])
+
+
+def get_arrays(file: Path):
+    """Read spectral data from h5 file and return as numpy arrays"""
+
+    rich.print(f"Read spectral data from {file}")
+
+    with h5.File(file, "r") as f:
+
+        # get axes and intensity
+        x = np.array(f.get("q_values"))
+        y = np.array(f.get("energy_values"))
+        try:
+            gz = np.array(f["spectral_function"])
+        except KeyError:
+            gz = np.array(f["intensity"])  # compatibility with older sqe.hdf5 files
+
+        xt = np.array(f.get("q_ticks"))
+        xl = f.attrs.get("q_tick_labels").decode().split()
+        yl = f"Energy ({f.attrs.get('energy_unit').decode():s})"
+
+    return data_spectral_cls(x, y, gz, xt, xl, yl)
+
+
+def get_arrays_dispersion(file: Path):
+    """Read dispersion data from h5 file and return as numpy arrays"""
+
+    rich.print(f"Read dispersion data from {file}")
+
+    with h5.File(file, "r") as f:
+
+        # get axes and intensity
+        x = np.array(f.get("q_values"))
+        y = np.array(f.get("frequencies"))
+
+    return data_dispersion_cls(x, y)
+
+
+def find_ylim(energy: np.ndarray, intensity: np.ndarray, max_frequency: float) -> float:
+    """find ylim as fraction of full band occupation"""
+    # integrate intensity in energy
+    n_bands = np.trapz(intensity, x=energy, axis=0).mean()
+    rich.print(f".. no. of bands:      {n_bands}")
+
+    for nn, yy in enumerate(energy):
+        gz_int = np.trapz(intensity[:nn], x=energy[:nn], axis=0).mean()
+        if gz_int > max_frequency * n_bands:
+            rich.print(f".. {max_frequency*100}% intensity at {yy:.3f} THz")
+            return yy
+
+    return energy.max()
