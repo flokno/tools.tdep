f3865e0266c0dda56c834933a7a68682e9939c2f
Florian Knoop <florian.knoop@liu.se>
2022-12-02 14:03:20 +0100

    ln_samples | don't break when there aren't any

--- tdeptools/cli/tdep_ln_previous_samples.py
+++ tdeptools/cli/tdep_ln_previous_samples.py
@@ -27,7 +27,10 @@ def main(
 
     folder_prev = next(Path("..").glob(f"{prefix}*{iter-1}")) / folder_name
     echo(f".. previous sample folder: {folder_prev}")
-    assert folder_prev.exists()
+
+    if not folder_prev.exists():
+        echo(f"** {folder_prev} does NOT exist, do NOT link")
+        return 1
 
     folder_new = Path(folder_name_new)
     echo(f"..                link to: {folder_new}")
