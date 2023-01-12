2768218be7a5d57c0768e9c033caed7c6775620c
Florian Knoop <florian.knoop@liu.se>
2023-01-12 15:23:28 +0100

    thermodynamics | make it work without gruneisen

--- tdeptools/cli/tdep_get_thermodynamics.py
+++ tdeptools/cli/tdep_get_thermodynamics.py
@@ -62,19 +62,6 @@ def main(
     except AttributeError:
         volume = read("infile.ucposcar", format="vasp").get_volume()
 
-    # sanity check gruneisen
-    g = ds.gruneisen_parameters
-    echo(f".. average Grueneisen parameter: {np.mean(g.data).round(decimals=14):.3f}")
-
-    if "gruneisen_tensors" in ds:
-        g_ab = ds.gruneisen_tensors
-        diff = np.linalg.norm(g - 1 / 3 * np.trace(g_ab, axis1=-1, axis2=-2))
-        echo(".. average Grueneisen tensor:    ")
-        g_ab_mean = np.mean(g_ab.data, axis=(0, 1)).round(decimals=14)
-        echo(g_ab_mean)
-        echo(f".. difference between Grueneisen parameters and tensors: {diff:.2e}")
-        echo(g_ab.shape)
-
     temperatures = np.linspace(mintemp, maxtemp, ntemp)
 
     # energies in eV
@@ -85,31 +72,63 @@ def main(
     cs_T = get_mode_heat_capacity(energies, temperatures)
     F_T = get_free_energy(energies, temperatures)
 
-    # mode gruneisen per temp
-    denom = cs_T.mean(axis=(0, 1))
-    denom[denom < k.lo_tiny] = k.lo_huge
-    g_T = (np.array(g)[:, :, None] * cs_T).mean(axis=(0, 1)) / denom
-
-    # quasiharmonic pressure
-    pV_ha_T = get_pressure_volume(energies, ds.gruneisen_parameters, temperatures)
-
-    # quasiharmonic stress
-    pV_ab_ha_T = get_stress_volume(energies, ds.gruneisen_tensors, temperatures)
-
-    # sanity check
-    assert np.allclose(pV_ha_T, np.mean(-pV_ab_ha_T[:, :3], axis=-1))
-
     index = pd.Index(data=temperatures, name="T [K]")
     data = {
         "F_ha(T) [eV]": F_T,
         "c_V(T) [meV]": 1e3 * c_T,
-        "pV(T) [eV]": pV_ha_T,
         "c_V/c_V_cl(T) [1]": c_T / cv,
-        "g(T) [1]": g_T,
     }
 
-    p_ha_T = pV_ha_T / volume
-    data["p_qha(T) [GPa]"] = p_ha_T
+    # sanity check gruneisen
+    if "gruneisen_parameters" in ds:
+        g = ds.gruneisen_parameters
+        echo(
+            f".. average Grueneisen parameter: {np.mean(g.data).round(decimals=14):.3f}"
+        )
+
+        # mode gruneisen per temp
+        denom = cs_T.mean(axis=(0, 1))
+        denom[denom < k.lo_tiny] = k.lo_huge
+        g_T = (np.array(g)[:, :, None] * cs_T).mean(axis=(0, 1)) / denom
+
+        # quasiharmonic pressure
+        pV_ha_T = get_pressure_volume(energies, ds.gruneisen_parameters, temperatures)
+
+        # quasiharmonic stress
+        pV_ab_ha_T = get_stress_volume(energies, ds.gruneisen_tensors, temperatures)
+
+        # sanity check
+        assert np.allclose(pV_ha_T, np.mean(-pV_ab_ha_T[:, :3], axis=-1))
+
+        g_ab = ds.gruneisen_tensors
+        diff = np.linalg.norm(g - 1 / 3 * np.trace(g_ab, axis1=-1, axis2=-2))
+        echo(".. average Grueneisen tensor:    ")
+        g_ab_mean = np.mean(g_ab.data, axis=(0, 1)).round(decimals=14)
+        echo(g_ab_mean)
+        echo(f".. difference between Grueneisen parameters and tensors: {diff:.2e}")
+        echo(g_ab.shape)
+
+        p_ha_T = pV_ha_T / volume
+
+        update = {"pV(T) [eV]": pV_ha_T, "g(T) [1]": g_T, '"p_qha(T) [GPa]"': p_ha_T}
+        data.update(update)
+
+        # qha stress for lattice optimization
+        echo("Dump stress")
+        index = pd.Index(data=temperatures, name="temperature")
+        data_stress = {
+            "xx": pV_ab_ha_T[:, 0] / volume,
+            "yy": pV_ab_ha_T[:, 1] / volume,
+            "zz": pV_ab_ha_T[:, 2] / volume,
+            "yz": pV_ab_ha_T[:, 3] / volume,
+            "xz": pV_ab_ha_T[:, 4] / volume,
+            "xy": pV_ab_ha_T[:, 5] / volume,
+        }
+        df = pd.DataFrame(data_stress, index=index)
+
+        outfile = "outfile.stress_qha.csv"
+        echo(f".. save to {outfile}")
+        df.to_csv(outfile, float_format=float_format)
 
     # create dataset
     echo("Dump data")
@@ -120,23 +139,6 @@ def main(
     pd.options.display.max_columns = 0  # automatic detection
     echo(df.head())
 
-    echo(f".. save to {outfile}")
-    df.to_csv(outfile, float_format=float_format)
-
-    # qha stress for lattice optimization
-    echo("Dump stress")
-    index = pd.Index(data=temperatures, name="temperature")
-    data = {
-        "xx": pV_ab_ha_T[:, 0] / volume,
-        "yy": pV_ab_ha_T[:, 1] / volume,
-        "zz": pV_ab_ha_T[:, 2] / volume,
-        "yz": pV_ab_ha_T[:, 3] / volume,
-        "xz": pV_ab_ha_T[:, 4] / volume,
-        "xy": pV_ab_ha_T[:, 5] / volume,
-    }
-    df = pd.DataFrame(data, index=index)
-
-    outfile = "outfile.stress_qha.csv"
     echo(f".. save to {outfile}")
     df.to_csv(outfile, float_format=float_format)
 
