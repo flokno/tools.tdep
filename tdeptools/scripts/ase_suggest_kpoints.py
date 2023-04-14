b7a2906920041a5cd761d312f9e95e7768c0dc85
Florian Knoop <florian.knoop@liu.se>
2023-04-14 09:13:08 +0200

    ase_suggest_kpoints | add resulting density (per direction)

--- tdeptools/scripts/ase_suggest_kpoints.py
+++ tdeptools/scripts/ase_suggest_kpoints.py
@@ -2,10 +2,19 @@
 
 from pathlib import Path
 
+import numpy as np
 import typer
-from ase.calculators.calculator import kptdensity2monkhorstpack
+from ase.calculators.calculator import kptdensity2monkhorstpack as density2k
 from ase.io import read
 from rich import print as echo
+
+
+def k2densities(atoms, k_grid):
+    """Generate the kpoint density per direction from given k_grid."""
+    recipcell = atoms.cell.reciprocal()
+    densities = np.asarray(k_grid) / (2 * np.pi * np.sqrt((recipcell ** 2).sum(axis=1)))
+    return densities
+
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
@@ -30,15 +39,18 @@ def main(
 
     atoms = read(file, format=format)
 
-    grid = kptdensity2monkhorstpack(atoms=atoms, kptdensity=density, even=even)
+    grid = density2k(atoms=atoms, kptdensity=density, even=even)
+    densities = k2densities(atoms=atoms, k_grid=grid)
 
-    echo(f"... structure:       {atoms}")
-    echo(f"... number of atoms: {len(atoms)}")
-    echo(f"... cell:")
-    echo(f"{atoms.cell[:]}")
-    echo(f"... density:         {density} per 1/Ang")
-    echo(f"... only even grids: {even}")
-    echo(f"--> grid: {grid}")
+    echo(f"... structure:          {atoms}")
+    echo(f"... number of atoms:    {len(atoms)}")
+    echo("... cell:")
+    echo(atoms.cell[:])
+    echo(f"... target density:     {density} per 1/Ang")
+    echo(f"... only even grids:    {even}")
+    echo(f"--> grid:               {grid}")
+    echo(f"--> resulting densites: {densities}")
+    echo(f"--> resulting density:  {densities.mean():.3f}")
 
 
 if __name__ == "__main__":
