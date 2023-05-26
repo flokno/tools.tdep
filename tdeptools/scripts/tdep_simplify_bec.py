f90b399708ce8e190c8d78a37049588f3bde99e3
Florian Knoop <florian.knoop@liu.se>
2023-05-26 11:09:05 +0200

    bec | default: original epsilon + scalar BEC, plus tweaks

--- tdeptools/scripts/tdep_simplify_bec.py
+++ tdeptools/scripts/tdep_simplify_bec.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 
+from pathlib import Path
+
 import numpy as np
 import typer
-
-
-outfile = "outfile.lotosplitting"
+from rich import print as echo
 
 app = typer.Typer()
 
@@ -19,9 +19,9 @@ def scalarize(array: np.ndarray) -> np.ndarray:
 
 
 @app.command()
-def main(file: str = "infile.lotosplitting"):
+def main(file: Path, outfile: Path = None):
     """Diagonalize the Born charges and make everything isotropic (scalar)"""
-    typer.echo(f"Read dielectric tensor and Born charges from {file}")
+    echo(f"Read dielectric tensor and Born charges from {file}")
 
     data = np.loadtxt(file).reshape(-1, 3, 3)
 
@@ -30,32 +30,49 @@ def main(file: str = "infile.lotosplitting"):
 
     bec_diag = np.zeros_like(bec)
     bec_scalar = np.zeros_like(bec)
+    bec_original = np.zeros_like(bec)
 
-    typer.echo(".. enforce acoustic sum rule")
+    echo(".. enforce acoustic sum rule")
     bec -= bec.mean(axis=0)
 
-    typer.echo(".. simplify BEC")
+    echo(".. simplify BEC")
     for (ii, ZZ) in enumerate(bec):
         bec_diag[ii] = diagonalize(ZZ)
         bec_scalar[ii] = scalarize(ZZ)
+        bec_original[ii] = ZZ
 
-    viol_diag = np.linalg.norm(bec_diag.mean(axis=0))
-    viol_scalar = np.linalg.norm(bec_scalar.mean(axis=0))
+    violation_scalar = np.linalg.norm(bec_scalar.mean(axis=0))
+    violation_diagonal = np.linalg.norm(bec_diag.mean(axis=0))
+    violation_original = np.linalg.norm(bec_original.mean(axis=0))
 
-    typer.echo(f".. acoustic sum rule violation for diagonal BEC: {viol_diag:.3e}")
-    typer.echo(f".. acoustic sum rule violation for isotropic BEC: {viol_scalar:.3e}")
+    echo(f".. acoustic sum rule violation for original BEC: {violation_original:.3e}")
+    echo(f".. acoustic sum rule violation for diagonal BEC: {violation_diagonal:.3e}")
+    echo(f".. acoustic sum rule violation for isotropic BEC: {violation_scalar:.3e}")
 
     eps_diag = diagonalize(eps)
     eps_scalar = scalarize(eps)
     new_bec_diag = np.concatenate([eps_diag[None, :], bec_diag]).reshape(-1, 3)
     new_bec_scalar = np.concatenate([eps_scalar[None, :], bec_scalar]).reshape(-1, 3)
+    new_bec_original = np.concatenate([eps[None, :], bec]).reshape(-1, 3)
 
-    typer.echo(f".. save   scalar BEC to {outfile}_scalar")
-    np.savetxt(outfile + "_scalar", new_bec_scalar)
-    typer.echo(f".. save diagonal BEC to {outfile}_diagonal")
-    np.savetxt(outfile + "_diagonal", new_bec_diag)
+    # also create non-diagonal epsilon with scalar BEC
+    new_bec_scalar_bec = new_bec_scalar.copy()
+    new_bec_scalar_bec[:3] = eps
 
-    typer.echo("done.")
+    if outfile is None:
+        outfile = "outfile.lotosplitting"
+
+    kw = {"fmt": "%22.15f"}
+    echo(f".. save original epsilon with   scalar BEC to {outfile}")
+    np.savetxt(outfile, new_bec_scalar_bec, **kw)
+    echo(f".. save   scalar epsilon with   scalar BEC to {outfile}_scalar")
+    np.savetxt(outfile + "_scalar", new_bec_scalar, **kw)
+    echo(f".. save   scalar epsilon with diagonal BEC to {outfile}_diagonal")
+    np.savetxt(outfile + "_diagonal", new_bec_diag, **kw)
+    echo(f".. save original epsilon with original BEC to {outfile}_original")
+    np.savetxt(outfile + "_original", new_bec_original, **kw)
+
+    echo("done.")
 
 
 if __name__ == "__main__":
