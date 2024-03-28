1816a1baa140b9ae78293f039faa518d6935fe8d
Florian Knoop <florian.knoop@liu.se>
2024-03-28 18:20:01 +0100

    displace_atoms | displace only unitcell atoms
    * only the unit cell atoms in a supercell, optional

--- tdeptools/scripts/tdep_displace_atoms.py
+++ tdeptools/scripts/tdep_displace_atoms.py
@@ -15,14 +15,45 @@ app = typer.Typer(pretty_exceptions_show_locals=False)
 @app.command()
 def main(
     file: Path,
+    file_unitcell: Path = None,
     plusminus: bool = True,
     displacement: float = typer.Option(default_displacement, help="displacement in Å"),
     format: str = "vasp",
 ):
-    """Displace the atoms"""
+    """Displace the atoms
+
+    If FILE_SUPERCELL is given, displace only the atoms at that position
+    """
+    import numpy as np
 
     echo(f"Read structure from {file}")
     atoms = read(file, format=format)
+
+    if file_unitcell is not None:
+        echo("... match to atoms in unit cell")
+        import json
+
+        atoms_uc = read(file_unitcell, format=format)
+
+        # which atom in the unit cell is which atom in the supercell?
+        watoms = atoms.copy()
+        # replace cell with that of unit cell
+        watoms.cell = atoms_uc.cell
+        list_match = []
+        for ii, spos in enumerate(watoms.get_scaled_positions(wrap=False)):
+            for _, upos in enumerate(atoms_uc.get_scaled_positions(wrap=False)):
+                if np.linalg.norm(spos - upos) < 1e-9:
+                    list_match.append(ii)
+        assert len(list_match) == len(atoms_uc), len(list_match)
+
+        echo(f"... matches: {list_match}")
+
+        _outfile = "uc_indices.json"
+        echo(f"... dump to {_outfile}")
+        json.dump(list_match, open(_outfile, "w"))
+
+    else:
+        list_match = np.arange(len(atoms))
 
     if plusminus:
         signs = (1, -1)
@@ -30,7 +61,7 @@ def main(
         signs = (1,)
 
     counter = 0
-    for nn in range(len(atoms)):
+    for nn in list_match:
         for ii in range(3):
             for ss in signs:
                 counter += 1
